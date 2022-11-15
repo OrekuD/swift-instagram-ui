1fe0cc34b37e21346eb51d6f3362d39f3d4d52a7
Cyboticx LLC <opokudavid446@gmail.com>
2022-11-15 11:38:13 +0000

    Init commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Cyboticx LLC on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
